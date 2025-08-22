b8329b7f7eb20386d02e7bdc863cc34776932159
Nesrine-blip <salahnesrine29@gmail.com>
2025-08-22 13:00:12 -0700

    creat new files

---
+++ src/services/API.js
@@ -0,0 +1,33 @@
+//#region - /* ===== CHECK FETCH SUPPORT ===== */
+if (!window.fetch) {
+  alert(
+    "Your browser does not support the Fetch API. Please update your browser."
+  );
+}
+//#endregion
+
+//================ GET ALL LOGEMENTS ================
+async function logementsGetAll() {
+  try {
+    const response = await fetch("/logements.json");
+    const data = await response.json();
+    console.log("✅ logements.json fetched successfully:", data); // ← Maintenant accessible
+    return data;
+  } catch (error) {
+    console.error("Error fetching logements:", error);
+  }
+}
+
+export { logementsGetAll };
+
+//================ GET LOGEMENT BY ID ================
+async function logementsGetById(id) {
+  try {
+    const logs = await logementsGetAll();
+    return logs.find(log => log.id === id);
+  } catch (error) {
+    console.error("Error fetching logement by ID:", error);
+  }
+}
+
+export { logementsGetById };
