6a89c771b909dbad709af54e19cfee4f210120c6
Nesrine-blip <salahnesrine29@gmail.com>
2025-08-29 20:59:04 -0700

    add collapse two

--- src/pages/LogementPage/LogementPage.jsx
+++ src/pages/LogementPage/LogementPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Carousel from "../../components/Carousel/Carousel.jsx";
 import Tags from "../../components/Tags/Tags.jsx";
 import Rating from "../../components/Rating/Rating.jsx"; 
+import Collapse from "../../components/Collapse/Collapse.jsx"; 
 import { logementsGetById } from "../../services/API.js";
 
 const LogementPage = () => {
@@ -26,43 +27,55 @@ const LogementPage = () => {
 
   return (
     <div className="logement-page">
+
+      {/* --- CAROUSEL --- */}
       <Carousel slides={logement.pictures} />
-      
+
+      {/* --- INFO PRINCIPALES ET HÔTE --- */}
       <div className="logement-info">
-        <h1 className="logement-title">{logement.title}</h1>
-        <p className="logement-location">{logement.location}</p>
-        
-        <div className="logement-tags">
-          {logement.tags.map((tag, index) => (
-            <Tags key={index} text={tag} />
-          ))}
+
+        {/* Bloc gauche : titre, localisation, tags */}
+        <div className="logement-main-info">
+          <h1 className="logement-title">{logement.title}</h1>
+          <p className="logement-location">{logement.location}</p>
+          <div className="logement-tags">
+            {logement.tags.map((tag, index) => (
+              <Tags key={index} text={tag} />
+            ))}
+          </div>
         </div>
-        
-        <div className="logement-host-rating-block">
+
+        {/* Bloc droit : hôte et rating */}
+        <div className="logement-host-block">
+          {/* Hôte : nom et photo */}
           <div className="logement-host-info">
             <h3>{logement.host.name}</h3>
             <img src={logement.host.picture} alt={logement.host.name} />
           </div>
+          {/* Rating : étoiles sous la photo */}
           <div className="logement-rating">
             <Rating rating={Number(logement.rating)} />
           </div>
         </div>
       </div>
 
+      {/* --- DÉTAILS DU LOGEMENT --- */}
       <div className="logement-details">
-        <div className="logement-description">
-          <h2>Description</h2>
-          <p>{logement.description}</p>
-        </div>
 
-        <div className="logement-equipments">
-          <h2>Équipements</h2>
+        {/* Bloc collapse : Description */}
+        <Collapse title="Description">
+          <p>{logement.description}</p>
+        </Collapse>
+
+        {/* Bloc collapse : Équipements */}
+        <Collapse title="Équipements">
           <ul>
             {logement.equipments.map((equip, index) => (
               <li key={index}>{equip}</li>
             ))}
           </ul>
-        </div>
+        </Collapse>
+
       </div>
     </div>
   );
